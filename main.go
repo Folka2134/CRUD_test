3d8eb1cfd7f560e55f0d71a7e17f105268e547d9
folka2134 <folka2134@gmail.com>
2024-02-22 10:47:35 +0000

    test for server

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/gin-contrib/cors"
@@ -14,9 +15,15 @@ func main() {
 		PORT = "8080"
 	}
 
+	http.HandleFunc("/", handler)
+
 	router := gin.New()
 	router.Use(gin.Logger())
 	router.Use(cors.Default())
 
 	log.Fatal(router.Run(":" + PORT))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(`{"message":"Hello, World!"}`))
 }
