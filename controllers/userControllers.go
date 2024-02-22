8207f94fbe7d576bc246af21848a7cc3ff1d0911
folka2134 <folka2134@gmail.com>
2024-02-22 11:38:27 +0000

    created user controllers

---
+++ controllers/userControllers.go
@@ -0,0 +1,39 @@
+package controllers
+
+import "github.com/gin-gonic/gin"
+
+func GetUser() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func CreateUser() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func UpdateUser() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func DeleteUser() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func UserSignup() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func UserLogin() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
