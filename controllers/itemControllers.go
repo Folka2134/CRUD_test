0ac3493eeb65165788bb5815a5d22c7ec2b2e203
folka2134 <folka2134@gmail.com>
2024-02-22 11:38:20 +0000

    created item controllers

---
+++ controllers/itemControllers.go
@@ -0,0 +1,33 @@
+package controllers
+
+import "github.com/gin-gonic/gin"
+
+func GetItems() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func GetItem() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func CreateItem() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func UpdateItem() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func DeleteItem() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
